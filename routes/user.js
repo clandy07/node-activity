7bcc1ee21d150f5d0b07518970f340632e8e4a9b
clandy07 <edelcortes3@gmail.com>
2024-10-05 02:52:12 +0800

    Added specialized rate limiter for register and login routes

--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,20 @@ const express = require('express');
 const { registerUser, loginUser, getUserProfile } = require('../controllers/userController');
 const authMiddleware = require('../middleware/authMiddleware');
 
+const rateLimit = require('express-rate-limit');
+
 const router = express.Router();
 
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 3, 
+    message: 'Too many registration or login attempts, please try again later.'
+  });
 
-router.post('/register', registerUser);
 
-router.post('/login', loginUser);
+router.post('/register', authLimiter, registerUser);
+
+router.post('/login', authLimiter, loginUser);
 
 router.get('/profile', authMiddleware, getUserProfile);
 
