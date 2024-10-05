cfb1bdd176b70ffe33d9c10004b5d756bfd388b2
clandy07 <edelcortes3@gmail.com>
2024-10-05 14:34:03 +0800

    Added comments for models

--- models/userModel.js
+++ models/userModel.js
@@ -2,43 +2,49 @@ const fs = require('fs');
 const path = require('path');
 
 
-const dataPath = path.join(__dirname, '../data/users.json');
+const dataPath = path.join(__dirname, '../data/users.json'); // Path to the JSON file where user data is stored
 
 
+// Function to read users from the JSON file
 const readUsers = () => {
     const fileData = fs.readFileSync(dataPath, 'utf-8');
     return JSON.parse(fileData || '[]');
 };
 
 
+// Function to save users to the JSON file
 const saveUsers = (users) => {
     fs.writeFileSync(dataPath, JSON.stringify(users, null, 2));
 };
 
 
+// Function to find a user by username
 const findByUsername = (username) => {
     const users = readUsers();
     return users.find(user => user.username === username);
 };
 
+// Function to find a user by email
 const findByEmail = (email) => {
     const users = readUsers();
     return users.find(user => user.email === email);
 };
 
+// Function to find a user by ID
 const findById = (id) => {
     const users = readUsers();
     return users.find(user => user.id === id);
 };
 
 
+// Function to add a new user
 const addUser = (newUser) => {
     const users = readUsers();
     users.push(newUser);
     saveUsers(users);
 };
 
-
+// Exporting all functions for use in other files
 module.exports = {
     findByUsername,
     findById,
--- models/validation.js
+++ models/validation.js
@@ -1,16 +1,17 @@
 const Joi = require('joi');
 
-
+ // Schema for validating user registration input
 const registerSchema = Joi.object({
-  username: Joi.string().alphanum().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).max(20).required()
+  username: Joi.string().alphanum().min(3).max(30).required(), // Username must be alphanumeric, 3-30 characters
+  email: Joi.string().email().required(), // Email must be a valid email format and is required
+  password: Joi.string().min(6).max(20).required() // Password must be 6-20 characters and is required
 });
 
 
+// Schema for validating user login input
 const loginSchema = Joi.object({
-  username: Joi.string().alphanum().required(),
-  password: Joi.string().min(6).max(20).required()
+  username: Joi.string().alphanum().required(), // Username must be alphanumeric and is required
+  password: Joi.string().min(6).max(20).required() // Password must be 6-20 characters and is required
 });
 
 module.exports = {
